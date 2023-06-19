7e76d701ab186e302f1864587d0274c11d57556e
aikan-umetsubo <yakinori21@gmail.com>
2023-06-20 03:44:27 +0900

    適当にReact

--- src/web/index.js
+++ src/web/index.js
@@ -1,4 +1,8 @@
-document.onload = () => {
-  const fs = new FileSystem();
-  
-}
+import { createRoot } from 'react-dom/client';
+
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world</h1>);
